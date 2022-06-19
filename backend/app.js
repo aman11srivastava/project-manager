9a014de4282b6f3c2f4ef2228518b8150ec534c0
Aman Srivastava <aman.srivastava@thoughtwizards.com>
2022-06-20 00:13:17 +0530

    Basic queries using sample data

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
 
 if (process.env.NODE_ENV !== "production") {
   dotenv.config({ path: "backend/config/config.env" });
 }
 const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
 
 module.exports = app;
