e934cd2c8ef111decb50336b0efdaa52c4708edf
Aman Srivastava <aman.srivastava@thoughtwizards.com>
2022-06-20 14:50:26 +0530

    Add Cors

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,13 @@ const express = require("express");
 const dotenv = require("dotenv");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const cors = require("cors");
 
 if (process.env.NODE_ENV !== "production") {
   dotenv.config({ path: "backend/config/config.env" });
 }
 const app = express();
+app.use(cors());
 
 app.use(
   "/graphql",
