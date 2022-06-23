ccc9f2be5cf5efee992f2d3ee04d37afcc5bc7fc
Aman Srivastava <aman.srivastava@thoughtwizards.com>
2022-06-23 13:56:41 +0530

    Delete Client

--- frontend/src/components/ClientRow.tsx
+++ frontend/src/components/ClientRow.tsx
@@ -2,20 +2,33 @@ import React from "react";
 import {ClientType} from "../utils/utils";
 import {Button} from "react-bootstrap";
 import {FaTrash} from "react-icons/fa";
+import {useMutation} from "@apollo/client";
+import {loader} from "graphql.macro";
 
 interface ClientRowProps {
     client: ClientType
 }
 
 export const ClientRow = (props: ClientRowProps) => {
-    const { name, phone, email} = props.client;
+    const { name, phone, email, id} = props.client;
+    const [deleteClient] = useMutation(loader('../graphql/deleteClient.graphql'))
+
+    function deleteClientHandler() {
+        deleteClient({
+            variables: {
+                id
+            },
+            refetchQueries: [{query: loader('../graphql/getClients.graphql')}]
+        })
+    }
+
     return (
         <tr>
             <td>{name}</td>
             <td>{email}</td>
             <td>{phone}</td>
             <td>
-                <Button className={"btn-danger btn-sm"}>
+                <Button onClick={deleteClientHandler} className={"btn-danger btn-sm"}>
                     <FaTrash/>
                 </Button>
             </td>
